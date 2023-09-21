cff573265f246af32b19543a77fedbec08dbfe81
Dale Lanto <dalelanto.work@gmail.com>
2023-09-21 19:58:57 +0800

    add the login and register pages

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -22,8 +22,8 @@ defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+   <input
+        class="border-gray-300 focus:border-orange-400 focus:ring-orange-400 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
         ref="input"
